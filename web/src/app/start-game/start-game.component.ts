78eea267353fa921f674b5ceae9b5af83db581db
sadhvibs <bssadhvi1999@gmail.com>
2025-04-27 20:29:04 +0530

    start tournament changes added

--- web/src/app/start-game/start-game.component.ts
+++ web/src/app/start-game/start-game.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SocketService } from '../service/socket.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-game',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./start-game.component.scss']
 })
 export class StartGameComponent {
+  room = '';
+  opponentName: any;
+  selectedRounds = 3;
 
+  constructor(private socketService: SocketService, private router: Router) { }
+
+  ngOnInit() {
+    this.room = localStorage.getItem('room') || '';
+    this.socketService.onTournamentStart((data: any) => {
+      this.opponentName = data.opponent;
+      // alert(`Your are playing against ${data.opponent}`)
+      sessionStorage.setItem('opponent', data.opponent)
+    })
+  }
+
+  onClickStart() {
+    this.socketService.startTournament(this.room, this.selectedRounds);
+    localStorage.setItem('rounds', this.selectedRounds.toString());
+    this.router.navigate(['/game'])
+  }
 }
